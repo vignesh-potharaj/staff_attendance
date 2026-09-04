79a99e064d3f536e17e8fd297bcd017cf357e381
vignesh-potharaj <23n31a2137@mrcet.ac.in>
2026-09-04 22:36:29 +0530

    feat(app): configure dual production and testing app product flavors and rename test app to Smart Staff (Test)

--- staff-portal/capacitor.config.ts
+++ staff-portal/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.smartstaff.app',
-  appName: 'Smart Staff',
+  appName: 'Smart Staff (Test)',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
